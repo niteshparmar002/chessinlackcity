542332a3bde1e8847cf305a181ce00d5480d7ae7
nitesh-segno <nitesh@segnotech.com>
2023-02-02 10:30:54 +0530

    changes

---
+++ src/Components/Home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className='container my-5'>
+            <h4 class="widget-title header-after1 ">
+                <span class="heading-line-before"></span>Featured Post<span class="heading-line-after"></span>
+            </h4>
+            <div className="row">
+                <div className="col-md-3">
+                    <img width="720" src="https://l3q225.n3cdn1.secureserver.net/wp-content/uploads/2022/01/Important-Annoucement-720x439.jpg" class="img-fluid" alt=""/>
+                    <h4>
+                        <a className='text-black m-0' href="https://aicf.in/circular-for-change-in-procedure-for-applying-under-the-scheme-of-cash-award-to-the-medal-winners-for-winning-medals-in-international-events-and-their-coaches-intimation-to-nsfs-list-2/">Circular for change in procedure for applying under the Scheme of Cash Award to the Medal Winners for winning medals in International Events and their coaches</a>
+                    </h4>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
